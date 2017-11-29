ad34bbec09465163e3d9c12247377b25fe8947bd
jjrv <juha@busfaster.com>
2017-11-29 08:50:38 +0200

    Improve dstore mobx adapter.

--- src/Mstore.ts
+++ src/Mstore.ts
@@ -7,13 +7,13 @@ import * as Promised from 'dstore/Promised';
 import * as SimpleQuery from 'dstore/SimpleQuery';
 import { IObservableArray } from 'mobx';
 import { IStateTreeNode } from 'mobx-state-tree';
-import { types as T, getPathParts, onSnapshot, onPatch } from 'mobx-state-tree';
+import { types as T, getPath, getPathParts, getParent, getRoot, getSnapshot, applyPatch, onSnapshot, onPatch } from 'mobx-state-tree';
 import * as Deferred from 'dojo/Deferred';
 import * as declare from 'dojo/_base/declare';
 
 export type Item = any;
 
-function wrapArray(data: any[], totalLength = data.length) {
+function wrapArray(data: any, totalLength = data.length) {
 	const Result: { new(): dstore.FetchArray<Item> } = function() {} as any;
 
 	Result.prototype = data;
@@ -26,41 +26,93 @@ function wrapArray(data: any[], totalLength = data.length) {
 
 const MstoreClass = declare([ Store, Promised, SimpleQuery ], {
 
-	constructor: function(tree: IStateTreeNode) {
+	constructor: function(tree: IStateTreeNode, storeIndex: any) {
 		this.tree = tree;
+
+		if(storeIndex) this.storeIndex = storeIndex;
+
+		this.storeIndex[this.getIdentity(tree)] = this;
 	},
 
 	data: null as any as any[],
 	tree: null as any as IStateTreeNode,
 	index: {} as { [id: string]: IStateTreeNode },
+	storeIndex: {} as any,
 
 	setData: function() {},
 
 	fetchSync: function(): dstore.FetchArray<Item> {
-		const list = this.tree as IObservableArray<any>;
+		const list = (this.tree as any).children as IObservableArray<any>;
 		return(wrapArray(list));
 	},
 
 	fetchRangeSync: function({ start, end }: { start: number, end: number }): dstore.FetchArray<Item> {
-		const list = this.tree as IObservableArray<any>;
+		const list = (this.tree as any).children as IObservableArray<any>;
 		return(wrapArray(list.slice(start, end), list.length));
 	},
 
 	addSync: function() {},
 
-	putSync: function(object: Item, options?: {}) {
-console.trace('putSync');
-console.log(object);
-console.log(options);
+	putSync: function(item: Item, options?: any) {
+		const id = options.beforeId;
+
+		let parentItem = getParent(getParent(item));
+
+		parentItem.detach(item);
+
+		this.storeIndex[
+			this.getIdentity(parentItem)
+		].emit(
+			'delete', {
+				target: item
+			}
+		);
+
+		let target: any;
+		let path: string;
+
+		if(id) {
+			target = this.index[id];
+			path = getPath(target);
+		} else {
+			target = item;
+			path = '/children/' + getRoot(this.tree).children.length;
+		}
+
+		applyPatch(this.tree, {
+			op: 'add',
+			path,
+			value: item
+		});
+
+		parentItem = getParent(getParent(target));
+
+		this.storeIndex[
+			this.getIdentity(parentItem)
+		].emit(
+			'add', {
+				beforeId: options.beforeId,
+				target: item
+			}
+		);
+
+		// TODO: emit update event if item was not moved.
 	},
 
 	getSync: function(id: any) {
-console.log('getSync');
-console.log(id);
 		return(this.index[id]);
 	},
 
-	removeSync: function(id: any) { return(true); },
+	removeSync: function(id: any) {
+		const item: Item = this.index[id];
+
+		let parent = getParent(item) as IObservableArray<Item>;
+		let pos = parent.indexOf(item);
+
+		parent.splice(pos, 1);
+
+		return(true);
+	},
 
 	getIdentity: function(item: Item) {
 		let id = item.id;
@@ -80,9 +132,6 @@ console.log(id);
 
 		this.index[id] = item;
 
-// console.log('getIdentity');
-// console.log(id);
-
 		return(id)
 	},
 
@@ -95,8 +144,7 @@ console.log(id);
 	},
 
 	getChildren: function(item: IStateTreeNode) {
-console.log('getChildren');
-		return(new Mstore((item as any).children));
+		return(new Mstore(item as any, this.storeIndex));
 	}
 
 }).createSubclass([ Trackable ]);
